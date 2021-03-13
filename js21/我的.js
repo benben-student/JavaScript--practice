22750eb66c1ada74e84e1ab042c51749ba778a19
520_forever <a441832620@126.com>
2021-03-13 17:57:49 +0800

    小练习

---
+++ js21/我的.js
@@ -0,0 +1,17 @@
+console.log(parseInt("10"));
+console.log(parseInt("10abc"));
+console.log(parseInt("sd4365"));
+
+console.log(parseFloat("10"));
+console.log(parseFloat("10.3abc"));
+console.log(parseFloat("sd4.365"));
+
+console.log(Number("10"));
+console.log(Number("10.3abc"));
+console.log(Number("sd4.365"));
+
+var a = 100;
+var b = 200;
+console.log(a.toString());
+console.log(typeof a.toString);
+
