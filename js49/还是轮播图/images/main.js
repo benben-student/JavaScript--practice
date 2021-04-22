6326a86a135feb20becd7c0d90446e3068b69513
520_forever <a441832620@126.com>
2021-04-22 19:39:54 +0800

    JS练习

---
+++ js49/还是轮播图/images/main.js
@@ -0,0 +1,18 @@
+function my$(id){
+    return document.getElementById(id);
+}
+function animate(element,target){
+    clearInterval(element.timeId);
+    element.timeId=setInterval(function(){
+        var current=element.offsetLeft;
+        var step = 10;
+        step=current<target?step:-step;
+        current+=step;
+        if(Math.abs(target-current)>Math.abs(step)){
+            element.style.left=current+"px";
+        }else{
+            clearInterval(element.timeId);
+            element.style.left=target+"px";
+        }
+    },5)
+}
