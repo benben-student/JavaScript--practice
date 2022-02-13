e2614b353642e8b7d1d1c2fbacbd6d38eec945dd
520-forever <a441832620@126.com>
2022-02-13 18:26:20 +0800

    new add

---
+++ js69/index2.js
@@ -0,0 +1,12 @@
+function animate(element,target){
+    var timeId=setInterval(function(){
+        var current=element.offsetLeft;
+        var temp=10;
+        current +=temp;
+        if(current<=target){
+            element.style.left=current+'px'
+        }else{
+            clearInterval(timeId);
+        }
+    },20)
+}
